5b42912897b8f3fb8014f0a9472b680fecf0d585
platnelson <isaacadams274@gmail.com>
2023-10-02 16:50:07 +1300

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -67,8 +67,8 @@ function regdone() {
     // check if it is empty.
    if (form.elements[i].type === "text" || form.elements[i].type === "email" ||
             form.elements[i].type === "tel") {
+              // If the element is empty, alert the user but do not reset the form.
           if (form.elements[i].value === "") {
-            // If the element is empty, alert the user but do not reset the form.
             alert("Please fill out all the required fields.");
             return false;
       }
@@ -76,22 +76,105 @@ function regdone() {
   }
 
   // If all the form elements are filled, allow the form to submit and display the "Done" message.
-  document.getElementById("reg").innerHTML = "Done!";
+  document.getElementById("reg").innerHTML = "Registration, successful!";
   return true;
+}
+
+
+function toggleNumPeopleInput(show) {
+  var numPeopleDiv = document.getElementById("numPeople");
+  var numPeopleInput = document.querySelector("input[name='numPeople']");
+  
+  if (show) {
+      numPeopleDiv.style.display = "block";
+      numPeopleInput.required = true;
+  } else {
+      numPeopleDiv.style.display = "none";
+      numPeopleInput.required = false;
+  }
+  calculateTotal();
+}
+
+function calculateTotal() {
+  var venueCosts = {
+      "The Hunter Lounge": 200,
+      "Victoria Venues": 250
+  };
+
+  var performerCosts = {
+      "magician": 45,
+      "clown": 55
+  };
+
+  var cateredCostPerPerson = 25;
+  
+  var selectedVenue = document.querySelector("select[name='venue']").value;
+  var selectedPerformer = document.querySelector("select[name='performer']").value;
+  var numPeople = parseInt(document.querySelector("input[name='numPeople']").value);
+  
+  var venueCost = venueCosts[selectedVenue] || 0;
+  var performerCost = performerCosts[selectedPerformer] || 0;
+  var cateredCost = document.querySelector("input[name='Catered']:checked").value === "Catered" ? cateredCostPerPerson * numPeople : 0;
+  
+  var totalCost = venueCost + performerCost + cateredCost;
+  
+  var totalElement = document.getElementById("total");
+  totalElement.textContent = "$" + totalCost;
 }
 
 function paydone() {
   // Get the form element.
   var form = document.getElementById("pay");
 
+  // Check the value of the "Catered" radio button.
+  var cateredRadio = document.querySelector("input[name='Catered']:checked");
+
+  if (cateredRadio && cateredRadio.value === "Catered") {
+    // "Catered" is selected, so validate the "numPeople" input.
+    var numPeopleInput = document.querySelector("input[name='numPeople']");
+
+    if (numPeopleInput.value === "") {
+      alert("Please fill out the required fields.");
+      return false;
+    }
+  }
+
+  // Iterate over all the other form elements.
+  for (var i = 0; i < form.elements.length; i++) {
+    // If the element is a text input, email input, date input, or select element,
+    // check if it is empty.
+    if (
+      form.elements[i].type === "date" ||
+      form.elements[i].type === "radio" ||
+      (form.elements[i].type === "number" && form.elements[i].name !== "numPeople")
+    ) {
+      // If the element is empty, alert the user but do not reset the form.
+      if (form.elements[i].value === "") {
+        alert("Please fill out all the required fields.");
+        return false;
+      }
+    }
+  }
+
+  // If all the form elements are filled, allow the form to submit and display the "Done" message.
+  document.getElementById("pay").innerHTML = "Payment, Successful!";
+  return true;
+}
+
+
+
+function faqdone() {
+  // Get the form element.
+  var form = document.getElementById("faqform");
+
   // Iterate over all the form elements.
   for (var i = 0; i < form.elements.length; i++) {
     // If the element is a text input, email input, date input, or select element,
     // check if it is empty.
-   if (form.elements[i].type === "text" || form.elements[i].type === "email" ||
-            form.elements[i].type === "tel") {
+   if (form.elements[i].type === "text" || form.elements[i].type === "tel" ||
+   form.elements[i].type === "textarea") {
+    // If the element is empty, alert the user but do not reset the form.
           if (form.elements[i].value === "") {
-            // If the element is empty, alert the user but do not reset the form.
             alert("Please fill out all the required fields.");
             return false;
       }
@@ -99,9 +182,12 @@ function paydone() {
   }
 
   // If all the form elements are filled, allow the form to submit and display the "Done" message.
-  document.getElementById("pay").innerHTML = "Done!";
+  document.getElementById("faqform").innerHTML = "Thanks!, We'll get abck to you as soon as possible.";
   return true;
 }
+
+
+
 // References //
 // Jemima. (n.d.). Floating Balloons. https://codepen.io/Jemimaabu/pen/vYEYdOy //
 
